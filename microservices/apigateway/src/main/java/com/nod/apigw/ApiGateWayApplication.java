5b205536f5fed6fb3716ebac9de913ac886d57c4
DinuwanB <dinubhagya97@gmail.com>
2023-02-25 02:11:20 +0530

    Rabbitmq added to docker compose and configured / api gateway module added / amqp module added

---
+++ microservices/apigateway/src/main/java/com/nod/apigw/ApiGateWayApplication.java
@@ -0,0 +1,14 @@
+package com.nod.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGateWayApplication.class, args);
+    }
+}
